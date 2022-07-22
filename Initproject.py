7b1b828acb3a9eaad66d684a7c5ef7deea110abb
Karol Kondracki <karxol@o2.pl>
2022-07-22 18:03:42 +0200

    new

---
+++ Initproject.py
@@ -0,0 +1,17 @@
+from multiprocessing.sharedctypes import Value
+
+print(' ')
+print(' ')
+print(' ')
+print(' ')
+
+sklep  = {
+    'Biedronka' : ['chleb','bulka','ciasto','cebula'],
+    'Zabka' : ['paluszki','hot-dog',]
+
+}
+
+for keys ,value  in sklep.items():
+
+    print(f'Ide do sklepu',keys,'i kupuje ',value ,' kupuję ',len(value), 'produkty',)
+    
